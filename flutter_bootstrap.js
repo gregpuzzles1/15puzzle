3b866648fb51339e5038a7f21b29e33ae268e9b3
gregpuzzles1 <gregpuzzles1@users.noreply.github.com>
2026-01-08 18:12:11 +0000

    deploy: 3f059ccd67e98b89bd0f000d5219f47075d89336

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2318129173"
+    serviceWorkerVersion: "2980652516"
   }
 });
